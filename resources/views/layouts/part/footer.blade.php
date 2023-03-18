01111a8e60438e635dfdde64cd27a77e5e70b177
vvvladimir758 <59477097+vvvladimir758@users.noreply.github.com>
2023-03-18 20:01:23 +0700

    доделка футера (адаптивность)

--- resources/views/layouts/part/footer.blade.php
+++ resources/views/layouts/part/footer.blade.php
@@ -1,15 +1,15 @@
 <div class="container-fluid footer">
 <div class="row  mt-3 ps-4 pe-4">
 
-<div class="col-auto col-md-auto  col-xs-8">
+<div class="col-auto col-md-auto  ">
  @include('layouts.part.agreements')
 </div>
 
-<div class="col text-center  col-xs-8">
+<div class="col  footer-logo">
  @include('layouts.part.logo')
 </div>
 
-<div class="col-auto col-md-auto  col-xs-8">
+<div class="col-auto col-md-auto  ">
  @include('layouts.part.socials_links')
   @include('layouts.part.copyright')
 </div>
