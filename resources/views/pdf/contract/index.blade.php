64f29595666a7d319fccc8a56160eed912be8cae
Anton <aner-anton@yandex.ru>
2023-04-30 21:41:01 +0300

    Исправления в шрифтах

--- resources/views/pdf/contract/index.blade.php
+++ resources/views/pdf/contract/index.blade.php
@@ -4,7 +4,7 @@
 	
 	<style type="text/css">
 
-	* {font-family: 'DejaVu Sans', 'Inter', sans-serif;font-size: 14px;line-height: 14px;}
+	* {font-family: 'DejaVu Sans', sans-serif;font-size: 14px;line-height: 14px;}
 	
 	</style>
 </head>
