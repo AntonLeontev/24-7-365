e8ddfa149865a168ddee3587ab99df8292335f58
Anton <aner-anton@yandex.ru>
2023-05-14 17:00:03 +0300

    Исправлено поведение при отказе от авторизации через соцсети

--- app/Http/Controllers/SocialsController.php
+++ app/Http/Controllers/SocialsController.php
@@ -3,20 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Actions\GetUserFromSocialsAction;
+use DomainException;
 use Laravel\Socialite\Facades\Socialite;
 use Laravel\Socialite\Two\InvalidStateException;
 
 class SocialsController extends Controller
 {
-    public function redirect($driver)
+    public function redirect(string $driver)
     {
+        if (request()->has('error') && request('error') === 'access_denied') {
+            return back();
+        }
+
+        if (request()->has('error')) {
+            throw new DomainException(request('error_description'), 1);
+        }
+
         return Socialite::driver($driver)->redirect();
     }
 
     public function callback(string $driver, GetUserFromSocialsAction $action)
     {
         try {
-			$user = $action($driver);
+            $user = $action($driver);
         } catch (InvalidStateException $e) {
             return back();
         }
