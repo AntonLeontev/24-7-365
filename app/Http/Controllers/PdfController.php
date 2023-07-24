cdb279a1247e7f8cc8be2d2bafeb1e62b44594ff
Anton <aner-anton@yandex.ru>
2023-07-24 13:41:26 +0300

    Добавлена оферта в пдф

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -9,16 +9,15 @@ class PdfController extends Controller
 {
     public function invoice(Payment $payment)
     {
-        //TODO delete
-        if (request()->is('invoices/{\d}/pdf/get')) {
-            return view('pdf.invoice', compact('payment'));
-        }
-        return Pdf::loadView('pdf.invoice', compact('payment'))->download('Счет.pdf');
+		$number = $payment->number;
+		$date = $payment->created_at;
+        return Pdf::loadView('pdf.invoice', compact('payment'))->download("Счет №{$number} от {$date->format('d.m.Y')}.pdf");
     }
 
     public function contract()
     {
         // return view('pdf.contract.index');
-        return Pdf::loadView('pdf.contract.index')->download('Оферта.pdf');
+        // return Pdf::loadView('pdf.contract.index')->download('Оферта.pdf');
+        return redirect(config('app.url') . '/Оферта.pdf');
     }
 }
